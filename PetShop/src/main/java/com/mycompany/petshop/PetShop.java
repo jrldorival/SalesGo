1a3c04328b58ed100a9d983ea3018ebb65259ee9
Dorival Junior <105865056+jrldorival@users.noreply.github.com>
2022-12-10 17:43:04 -0300

    Add files via upload
    Adicionado autores

--- PetShop/src/main/java/com/mycompany/petshop/PetShop.java
+++ PetShop/src/main/java/com/mycompany/petshop/PetShop.java
@@ -7,11 +7,16 @@ package com.mycompany.petshop;
 
 /**
  *
- * @author GABRIELAOLIVEIRAMONT
+ * @author DORIVAL LOMBISANI JUNIOR, 1806805 
+           FÁBIO ANTONIO FERREIRA, 1820105 
+           FABIO IOVENI LAVANDOSCKI, 1812108 
+           JULIANO DOS SANTOS, 1806481 
+           NICOLAS VOGIANTZIS, 1820026 
+           RENAN RODRIGUES
  */
 public class PetShop {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("SalesGo!!");
     }
 }
